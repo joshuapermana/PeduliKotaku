ca4a12387f752d7d0726f1fb61aa0bb6cc7b16b7
Joshua Permana <joshuapermana01@gmail.com>
2017-12-11 00:14:14 +0700

    Setting Router

--- src/Router/MainRouter.js
+++ src/Router/MainRouter.js
@@ -1,1 +1,11 @@
 //Router
+import React, { Component } from "react";
+import MainScreenNavigator from "../Screen/index.js";
+import { DrawerNavigator } from "react-navigation";
+
+const MainRouter = DrawerNavigator(
+  {
+    Page: { screen: MainScreenNavigator }
+  },
+);
+export default MainRouter;
